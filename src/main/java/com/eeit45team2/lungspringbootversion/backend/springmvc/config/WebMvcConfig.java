e82e524e8e3fa85a9bfb0b6ca7d0c7f04b3ec854
homieyang <homieyang@outlook.com>
2022-06-27 22:36:18 +0800

    CRUD基本完成，但是刪除的SweetAlert做不出來

--- src/main/java/com/eeit45team2/lungspringbootversion/backend/springmvc/config/WebMvcConfig.java
+++ src/main/java/com/eeit45team2/lungspringbootversion/backend/springmvc/config/WebMvcConfig.java
@@ -1,15 +1,7 @@
 package com.eeit45team2.lungspringbootversion.backend.springmvc.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.multipart.commons.CommonsMultipartResolver;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
-import org.springframework.web.servlet.view.JstlView;
 
 @Configuration
 
@@ -17,8 +9,8 @@ public class WebMvcConfig implements WebMvcConfigurer {
 //    @Override
 //    public void addResourceHandlers(ResourceHandlerRegistry registry) {
 //        registry
-//            .addResourceHandler("/URLToReachResourcesFolder/**")
-//            .addResourceLocations("/resources/");
+//                .addResourceHandler("/URLToReachResourcesFolder/**")
+//                .addResourceLocations("/BackEnd/");
 //        registry.addResourceHandler("/localImage/**").addResourceLocations("/resources/images/memberHeadshot/");
 //        // 專案/headshot
 //        // 專案/ member /list
