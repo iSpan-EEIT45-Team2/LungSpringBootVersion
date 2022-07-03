ae9518bbba9b517c1b6d88e7a353e505cf6a07e0
homieyang <homieyang@outlook.com>
2022-07-03 22:31:03 +0800

    dev

--- src/main/java/com/eeit45team2/lungspringbootversion/Frontend/Product/repo/ItemRepo.java
+++
@@ -1,7 +0,0 @@
-package com.eeit45team2.lungspringbootversion.Frontend.Product.repo;
-
-import com.eeit45team2.lungspringbootversion.Frontend.Product.model.ItemBean;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ItemRepo extends JpaRepository<ItemBean, Long> {
-}
--- src/main/java/com/eeit45team2/lungspringbootversion/Frontend/Product/repo/ProductRepo.java
+++
@@ -1,7 +0,0 @@
-package com.eeit45team2.lungspringbootversion.Frontend.Product.repo;
-
-import com.eeit45team2.lungspringbootversion.Frontend.Product.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepo extends JpaRepository<Product, Long> {
-}
