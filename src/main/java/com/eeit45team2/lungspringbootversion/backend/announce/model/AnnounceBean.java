4f435c5ff8f845e9e6fd68d462d29b93f65b4c77
tingyu <amy5598755987@gmail.com>
2022-07-06 12:48:03 +0800

    six

--- src/main/java/com/eeit45team2/lungspringbootversion/backend/announce/model/AnnounceBean.java
+++ src/main/java/com/eeit45team2/lungspringbootversion/backend/announce/model/AnnounceBean.java
@@ -21,7 +21,7 @@ public class AnnounceBean {
 	String anContent;
 	String anType;
 	String anEditor;
-//	Date anDate;
+
 
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "GMT+8")
 	Date anDate;
@@ -31,7 +31,7 @@ public class AnnounceBean {
 	}
 
 	public AnnounceBean(String anTitle, String anContent, String anType, String anEditor,
-						 Date anDate) {
+						Date anDate) {
 		super();
 		this.anTitle = anTitle;
 		this.anContent = anContent;
@@ -51,58 +51,59 @@ public class AnnounceBean {
 		this.anDate = anDate;
 	}
 
-//	public AnnounceBean(Long anNo) {
-//		this.anNo = anNo;
-//	}
+	public AnnounceBean(Long anNo) {
+		this.anNo = anNo;
+	}
+
+	public Long getAnNo() {
+		return anNo;
+	}
 //
-//	public Long getAnNo() {
-//		return anNo;
-//	}
+// public void setAnNo(Long anNo) {
+//    this.anNo = anNo;
+// }
 //
-//	public void setAnNo(Long anNo) {
-//		this.anNo = anNo;
-//	}
+// public String getAnTitle() {
+//    return anTitle;
+// }
 //
-//	public String getAnTitle() {
-//		return anTitle;
-//	}
+// public void setAnTitle(String anTitle) {
+//    this.anTitle = anTitle;
+// }
 //
-//	public void setAnTitle(String anTitle) {
-//		this.anTitle = anTitle;
-//	}
+// public String getAnContent() {
+//    return anContent;
+// }
 //
-//	public String getAnContent() {
-//		return anContent;
-//	}
+// public void setAnContent(String anContent) {
+//    this.anContent = anContent;
+// }
 //
-//	public void setAnContent(String anContent) {
-//		this.anContent = anContent;
-//	}
+// public String getAnType() {
+//    return anType;
+// }
 //
-//	public String getAnType() {
-//		return anType;
-//	}
+// public void setAnType(String anType) {
+//    this.anType = anType;
+// }
 //
-//	public void setAnType(String anType) {
-//		this.anType = anType;
-//	}
+// public String getAnEditor() {
+//    return anEditor;
+// }
 //
-//	public String getAnEditor() {
-//		return anEditor;
-//	}
+// public void setAnEditor(String anEditor) {
+//    this.anEditor = anEditor;
+// }
 //
-//	public void setAnEditor(String anEditor) {
-//		this.anEditor = anEditor;
-//	}
+// public Date getAnDate() {
+//    return anDate;
+// }
 //
-//	public Date getAnDate() {
-//		return anDate;
-//	}
-//
-//	public void setAnDate(Date anDate) {
-//		this.anDate = anDate;
-//	}
+// public void setAnDate(Date anDate) {
+//    this.anDate = anDate;
+// }
 
 
 
 }
+
