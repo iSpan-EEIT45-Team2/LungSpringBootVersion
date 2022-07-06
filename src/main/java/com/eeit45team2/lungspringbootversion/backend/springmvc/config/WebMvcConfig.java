55342409a69692e0abf2f25d35618b3f05c21483
homieyang <homieyang@outlook.com>
2022-07-06 23:04:04 +0800

    確認各項功能已完成,商品缺圖片顯示

--- src/main/java/com/eeit45team2/lungspringbootversion/backend/springmvc/config/WebMvcConfig.java
+++ src/main/java/com/eeit45team2/lungspringbootversion/backend/springmvc/config/WebMvcConfig.java
@@ -2,7 +2,6 @@ package com.eeit45team2.lungspringbootversion.backend.springmvc.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -18,7 +17,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
 //        // 專案/ member /list
 //    }
 
-//    @Override
+    //    @Override
 //	public void addViewControllers(ViewControllerRegistry registry) {
 //        registry.addViewController("file:src/main/resources/templates/login.html").setViewName("login");
 //
@@ -28,15 +27,17 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         //registry.addResourceHandler("/image/**").addResourceLocations("file:user-photos/");
-        registry.addResourceHandler("/image/**","/image3/**").addResourceLocations("file:src/main/resources/static/BackEnd/images/animal/"
-            ,"file:src/main/resources/static/BackEnd/images/activity/");
+        registry.addResourceHandler("/image/**", "/image3/**").addResourceLocations("file:src/main/resources/static/BackEnd/images/animal/"
+                , "file:src/main/resources/static/BackEnd/images/activity/");
         //path name                            //real path date.jpg
- //       registry.addResourceHandler("productpicture/**").addResourceLocations("file:src/main/resources/static/FrontEnd/images/product/");
+        //       registry.addResourceHandler("productpicture/**").addResourceLocations("file:src/main/resources/static/FrontEnd/images/product/");
 
         registry.addResourceHandler("image4/**").addResourceLocations("file:src/main/resources/static/BackEnd/images/product/");
 
-}
         registry.addResourceHandler("/productImage/**").addResourceLocations("file:src/main/resources/static/FrontEnd/images/Product/");
         registry.addResourceHandler("/memberHeadshotDir/**").addResourceLocations("file:src/main/resources/static/BackEnd/images/memberHeadshot/");
+
     }
+
+}
 
