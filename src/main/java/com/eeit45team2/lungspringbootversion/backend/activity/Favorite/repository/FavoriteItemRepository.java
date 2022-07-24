52de4c5926ff682e29e1a9f5d472b351e0ceaada
Ethan-98 <tomshen1025@gmail.com>
2022-07-24 22:51:33 +0800

    完成FrontIndex.html志工活動部分

--- src/main/java/com/eeit45team2/lungspringbootversion/backend/activity/Favorite/repository/FavoriteItemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.eeit45team2.lungspringbootversion.backend.activity.Favorite.repository;
-
-import com.eeit45team2.lungspringbootversion.backend.activity.Favorite.model.FavoriteItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FavoriteItemRepository extends JpaRepository<FavoriteItem, Long> {
-}
